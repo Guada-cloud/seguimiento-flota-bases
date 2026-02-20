ad1ff84ff6921ca18661ac991c7a38ba069059f2
Guada-cloud <gzalazar@redsos.com.ar>
2026-02-19 22:41:12 -0300

    Create utils_ops.py

---
+++ utils_ops.py
@@ -0,0 +1,6 @@
+from utils_ops import (
+    EXPECTED_PLAN, EXPECTED_REAL, _guess_map, apply_map, enrich_time,
+    merge_plan_real, compute_metrics, agg_error_metrics, add_time_keys,
+    filter_df, top5_hours, worst_base,
+    save_csv, load_csv, PLAN_CSV, REAL_CSV, MERG_CSV, to_excel_bytes
+)
